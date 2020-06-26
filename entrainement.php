dd065b891e520007d2ff9839cc17bdda5ef19f29
MarJose-Car <maria-jose.carvalho@lepoles.com>
2020-06-26 10:02:35 +0200

    wip

--- entrainement.php
+++ entrainement.php
@@ -264,12 +264,12 @@ echo "Le type d'une opération entre un INTEGER et un STRING est :". gettype($nu
 
 $c = 50; $d = 25;
 
-$c += $d      // équivaut à écrire $c = $c + $d;  => affiche 75
+$c += $d;      // équivaut à écrire $c = $c + $d;  => affiche 75
 
-$c -= $d      // équivaut à écrire $c = $c - $d;  => affiche 25
+$c -= $d;     // équivaut à écrire $c = $c - $d;  => affiche 25
 
-$c *= $d      // équivaut à écrire $c = $c * $d;  => affiche 1250
+$c *= $d;      // équivaut à écrire $c = $c * $d;  => affiche 1250
 
-$c /= $d      // équivaut à écrire $c = $c / $d;  => affiche 2
+$c /= $d;      // équivaut à écrire $c = $c / $d;  => affiche 2
 
-$c %= $d      // équivaut à écrire $c = $c % $d;  => affiche 0
+$c %= $d;      // équivaut à écrire $c = $c % $d;  => affiche 0
